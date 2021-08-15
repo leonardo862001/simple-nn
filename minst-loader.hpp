fd5f6a3bd2c7deb3dd9d4395d29903c359144364
Leonardo Battista <leonardo862001@gmail.com>
2021-08-15 18:57:50 +0200

    Initial Commit

---
+++ minst-loader.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <fstream>
+#include <iostream>
+#include <Eigen/Core>
+#include <tuple>
+const std::string training_image_fn = "mnist/train-images.idx3-ubyte";
+const std::string training_label_fn = "mnist/train-labels.idx1-ubyte";
+std::ifstream image;
+std::ifstream label;
+
+void print_image(Eigen::VectorXd im){
+	for(int i = 0; i < 28*28 ; i++){
+		if(i%28 == 0) std::cout << std::endl;
+		std::cout << (im(i)==0?0:1);
+	}
+}
+
+void init_minst() {
+	image.open(training_image_fn.c_str(), std::ios::in | std::ios::binary);
+	label.open(training_label_fn.c_str(), std::ios::in | std::ios::binary);
+		char number;
+		for (int i = 1; i <= 16; ++i) {
+			image.read(&number, sizeof(char));
+		}
+		for (int i = 1; i <= 8; ++i) {
+			label.read(&number, sizeof(char));
+		}
+	}
+std::tuple<Eigen::VectorXd, Eigen::VectorXd> get_image(){
+	const int height = 28;
+	const int width = 28;
+	Eigen::VectorXd data(height*width);
+	// Reading image
+	char number;
+	for (int i = 0; i < height; ++i) {
+		for (int j = 0; j < width; ++j) {
+			image.read(&number, sizeof(char));
+			data(i*width+j) = (uint8_t)(number)/255.0;
+		}
+	}
+
+	label.read(&number, sizeof(char));
+	Eigen::VectorXd expected(10);
+	for(int i = 0; i < 10; i++){
+		if((int) number == i)
+			expected(i) = 1.0;
+		else
+			expected(i) = 0.0;
+	}
+	print_image(data);
+	std::cout << std::endl;
+	std::cout << "label: " << (int) number << std::endl;
+	std::tuple<Eigen::VectorXd, Eigen::VectorXd> res = std::make_tuple(data, expected);
+	return res;
+}
+void close_minst(){
+	image.close();
+	label.close();
+}
+
---
+++ network.cpp
@@ -0,0 +1,98 @@
+#include <Eigen/Core>
+
+#include "minst-loader.hpp"
+using namespace Eigen;
+
+VectorXd sigmoid(VectorXd x) { return 1 / (1 + ((-x).array()).exp()); }
+class network {
+       public:
+	network(int i, int h, int o) {
+		w0 = MatrixXd::Random(h, i);
+		w1 = MatrixXd::Random(o, h);
+		b0 = VectorXd::Zero(h);
+		b1 = VectorXd::Zero(o);
+	}
+	VectorXd feedforward(VectorXd input) {
+		a1 = w0 * input + b0;
+		a1 = sigmoid(a1);
+		a2 = w1 * a1 + b1;
+		a2 = sigmoid(a2);
+		return a2;
+	}
+	MatrixXd w0, w1;
+	VectorXd a1, a2;
+	VectorXd b0, b1;
+};
+
+class trainer {
+       public:
+	trainer(network& net) : _net(net) {
+		init_minst();
+	}
+	~trainer() { close_minst(); }
+	float cost(VectorXd y, VectorXd expected) {
+		return 0.5 * (expected - y).squaredNorm();
+	}
+	float batch_cost(int batch_size, VectorXd* inputs, VectorXd* outputs) {
+		float e = 0;
+		for (int i = 0; i < batch_size; i++) {
+			e += cost(outputs[i], inputs[i]);
+		}
+		return e / batch_size;
+	}
+	void backpropagation(VectorXd expected, VectorXd input) {
+		VectorXd yd(expected.rows());
+		yd = _net.a2.array() * (1-_net.a2.array()) * (_net.a2-expected).array();
+		VectorXd hd(_net.a1.rows());
+		hd = (_net.w1.transpose()*yd).array()*(_net.a1.array() * (1-_net.a1.array())).array();
+		delta2 = yd*_net.a1.transpose();
+		_net.w1 -= eta*delta2;
+		_net.b1 -= eta*yd;
+		delta1 = hd*input.transpose();
+		_net.w0 -= eta*delta1;
+		_net.b0 -= eta*hd;
+	}
+	void learn(){
+		for(int i = 0; i < 500; i++){
+			std::cout << "Batch: " << i << std::endl;
+			for(int j=0; j < 100; j++){
+				std::cout << "Sample: " << j << std::endl;
+				auto im = get_image();
+				VectorXd y = _net.feedforward(std::get<0>(im));
+				std::cout << y.transpose() << std::endl;
+				backpropagation(std::get<1>(im),std::get<0>(im));
+			}
+		}
+	}
+	void test(){
+		std::cout << "testing: " << std::endl;
+		double ok = 0.0;
+		for(int i = 0; i < 10000; i++){
+			auto x = get_image();
+			VectorXd y = _net.feedforward(std::get<0>(x));
+			int max_i = 0;
+			for(int i = 0; i < 10; i++){
+				if(y(i) > y(max_i)) max_i = i;
+			}
+			int max_i_c = 0;
+			for(int i = 0; i < 10; i++){
+				if(std::get<1>(x)(i) > std::get<1>(x)(max_i_c)) max_i_c = i;
+			}
+			std::cout << "\nresult: " << max_i << std::endl;
+			if(max_i == max_i_c) ok+=1;
+
+		}
+		std::cout << "Accuracy: " << ok/10000 << std::endl;
+	}
+	network& _net;
+	const double eta = 0.32;
+	const double momentum = 0.9;
+	MatrixXd delta1, delta2;
+};
+
+int main(){
+	network nn(784, 30, 10);
+	trainer tr(nn);
+	tr.learn();
+	tr.test();
+}
