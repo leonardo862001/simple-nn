207ec132d53aee75798fa3383d7f9213cdd8d145
Leonardo Battista <leonardo862001@gmail.com>
2021-08-16 12:23:18 +0200

    added the possibility to use an arbitrary number of layers

--- network.cpp
+++ network.cpp
@@ -1,33 +1,50 @@
-#include <Eigen/Core>
+#include<Eigen/Core>
+#include<initializer_list>
+#include<vector>
 
 #include "mnist-loader.hpp"
 using namespace Eigen;
 
 VectorXd sigmoid(VectorXd x) { return 1 / (1 + ((-x).array()).exp()); }
+VectorXd dsigmoid(VectorXd x) { return sigmoid(x).array()*(1-sigmoid(x).array()); }
+VectorXd ReLU(VectorXd x) { auto f = [](double k)->double{ return k<0?0:k; }; return x.unaryExpr(f); }
+VectorXd dReLU(VectorXd x) { auto f = [](double k)->double{ return k<=0?0:1; }; return x.unaryExpr(f); }
+VectorXd htangent(VectorXd x) { return x.array().tanh(); }
+VectorXd dhtangent(VectorXd x) { return 1-x.array().tanh().square(); }
+auto activation = sigmoid;
+auto dactivation = dsigmoid;
 class network {
        public:
-	network(int i, int h, int o) {
-		w0 = MatrixXd::Random(h, i);
-		w1 = MatrixXd::Random(o, h);
-		b0 = VectorXd::Zero(h);
-		b1 = VectorXd::Zero(o);
+	network(std::initializer_list<int> l) {
+		for( auto i : l ){
+			b.push_back(VectorXd::Zero(i));
+			a.push_back(VectorXd::Zero(i));
+		}
+		w.push_back(MatrixXd::Zero(1,1));
+		for(int i = 1; i < a.size(); i++){
+			w.push_back(MatrixXd::Random(a[i].rows(), a[i-1].rows()));
+		}
 	}
 	VectorXd feedforward(VectorXd input) {
-		a1 = w0 * input + b0;
-		a1 = sigmoid(a1);
-		a2 = w1 * a1 + b1;
-		a2 = sigmoid(a2);
-		return a2;
+		a[0] = input;
+		int i;
+		a[1] = w[1]*a[0]+b[1];
+		for(i = 1; i < a.size()-1; i++){
+			a[i+1] = w[i+1] * activation(a[i])+b[i+1];
+		}
+		return activation(a[i]);
 	}
-	MatrixXd w0, w1;
-	VectorXd a1, a2;
-	VectorXd b0, b1;
+	std::vector<MatrixXd> w;
+	std::vector<VectorXd> a;
+	std::vector<VectorXd> b;
 };
 
 class trainer {
        public:
 	trainer(network& net) : _net(net) {
 		init_minst();
+		for( auto i : _net.w )
+			v.push_back(MatrixXd::Zero(i.rows(), i.cols()));
 	}
 	~trainer() { close_minst(); }
 	float cost(VectorXd y, VectorXd expected) {
@@ -41,16 +58,20 @@ class trainer {
 		return e / batch_size;
 	}
 	void backpropagation(VectorXd expected, VectorXd input) {
-		VectorXd yd(expected.rows());
-		yd = _net.a2.array() * (1-_net.a2.array()) * (_net.a2-expected).array();
-		VectorXd hd(_net.a1.rows());
-		hd = (_net.w1.transpose()*yd).array()*(_net.a1.array() * (1-_net.a1.array())).array();
-		delta2 = yd*_net.a1.transpose();
-		_net.w1 -= eta*delta2;
-		_net.b1 -= eta*yd;
-		delta1 = hd*input.transpose();
-		_net.w0 -= eta*delta1;
-		_net.b0 -= eta*hd;
+		std::vector<VectorXd> delta;
+		for( auto i : _net.a ){
+			delta.push_back(VectorXd::Zero(i.rows()));
+		}
+		delta.back() = dactivation(_net.a.back()).array() * (activation(_net.a.back())-expected).array();
+		v.back() = mu*v.back()- eta*delta.back()*activation(_net.a[delta.size()-2]).transpose();
+		_net.w.back() += v.back();
+		_net.b.back() -= eta*delta.back();
+		for(int i = delta.size()-2; i > 0; i--){
+			delta[i] = (_net.w[i+1].transpose()*delta[i+1]).array()*dactivation(_net.a[i]).array();
+			v[i] =mu*v[i] - eta*delta[i]*activation(_net.a[i-1]).transpose();
+			_net.w[i] += v[i];
+			_net.b[i] -= eta*delta[i];
+		}
 	}
 	void learn(){
 		for(int i = 0; i < 500; i++){
@@ -82,16 +103,17 @@ class trainer {
 			if(max_i == max_i_c) ok+=1;
 
 		}
-		std::cout << "Accuracy: " << ok/10000 << std::endl;
+		std::cout << "Accuracy: " << ok/10000 *100<< "%" << std::endl;
 	}
 	network& _net;
-	const double eta = 0.32;
-	const double momentum = 0.9;
+	const double eta = 0.065;
+	const double mu = 0.78;
 	MatrixXd delta1, delta2;
+	std::vector<MatrixXd> v;
 };
 
 int main(){
-	network nn(784, 30, 10);
+	network nn({784, 16, 16, 10});
 	trainer tr(nn);
 	tr.learn();
 	tr.test();
